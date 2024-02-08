0e55c4fc0ccad43c7be9e14065086fb1c9f62201
rrenn45 <rrenn45@gmail.com>
2024-02-08 15:43:05 -0800

    metadata updated

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,10 @@
 import CustomersTable  from '@/app/ui/customers/table';
 import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
 
 export default async function Page({
     searchParams,
