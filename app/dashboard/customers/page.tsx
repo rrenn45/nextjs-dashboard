3cb3fdf0553a9140729cc90e42f2e76fba9430e0
rrenn45 <rrenn45@gmail.com>
2024-02-08 14:55:10 -0800

    customer page built

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,14 @@
-export default function Page(){
-    return <p>Customers page</p>
+import CustomersTable  from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export default async function Page({
+    searchParams,
+  }: {
+    searchParams?: {
+      query?: string;
+      page?: string;
+    };
+  }){
+    const query = searchParams?.query || '';
+    return <CustomersTable query={query} />
 }
