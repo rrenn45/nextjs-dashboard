1b2be4f7ddd86120124867e68e31c179afac3042
rrenn45 <rrenn45@gmail.com>
2024-08-08 19:36:26 -0700

    Update nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,6 +4,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  WrenchScrewdriverIcon
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -19,6 +20,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  {name: 'Work Orders', href: '/dashboard/workorders', icon: WrenchScrewdriverIcon}
 ];
 
 export default function NavLinks() {
